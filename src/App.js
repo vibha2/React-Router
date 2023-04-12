6d3896c08cc12ee847314de3e1f133e783ffefa0
vibha2 <juhisahu2203@gmail.com>
2023-04-12 12:30:50 +0530

    Nested Routing Concept done with Outlet Tag

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import './App.css';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Home from "./components/Home";
+import Support from "./components/Support";
+import About from "./components/About";
+import Labs from "./components/Labs";
+import NotFound from './components/NotFound';
+import MainHeader from './components/MainHeader';
+// import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      App
+
+    <nav>
+    <ul>
+      <li>
+      <NavLink to="/">Home</NavLink>
+      </li>
+
+      <li>
+      <NavLink to="/about">About</NavLink>
+      </li>
+
+      <li>
+      <NavLink to="/support">Support</NavLink>
+      </li>
+
+      <li>
+      <NavLink to="/labs">Labs</NavLink>
+      </li>
+      
+    </ul>
+
+    </nav>
+
+    <Routes>
+        <Route path="/" element={<MainHeader/>} > 
+          <Route index element={<Home/>} /> 
+          {/* index means - default route */}
+          <Route path="/support" element={<Support/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/labs" element={<Labs/>} />
+          <Route path="*" element={<NotFound/>} />
+        </Route>
+    </Routes>
+      
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+
+//rafce
+
+const Home = () => {
+  return (
+    <div>
+      This is my Home Page
+    </div>
+  )
+}
+
+export default Home;
